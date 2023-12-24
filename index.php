c54cceae014752d9bde4612d10ee88d4dddddce0
Andrew <frintu.andrei07@gmail.com>
2023-12-24 13:09:09 +0200

    main structure

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ro">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Târgu Jiu - Capitala Tineretului</title>
+</head>
+<body>
+    
+</body>
+</html>
